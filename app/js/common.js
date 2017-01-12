521ce2980bbf8da67d86e1d099fcb4a7241e4693
joraSir <siriye@mail.ru>
2017-01-12 10:20:58 +0200

    +loader mixin +burger menu-button mixin

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 
+    $(window).load(function() {
+        $(".loader").delay(400).fadeOut("slow");
+    });
+
     svg4everybody({});
+
+    $(".menu-button").click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass("is-active");
+        $(".top_nav").slideToggle();
+    });
 });
